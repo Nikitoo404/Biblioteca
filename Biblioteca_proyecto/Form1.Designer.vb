7284b3c5092524c33c2ddd041112ec3d66e2385b
Nicolas <impresonico@gmail.com>
2024-10-19 05:25:52 -0300

    Initial commit

---
+++ Biblioteca_proyecto/Form1.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PnlBarra = New System.Windows.Forms.Panel()
+        Me.LblBienvenida = New System.Windows.Forms.Label()
+        Me.BtnMin = New System.Windows.Forms.Button()
+        Me.BtnCerrar = New System.Windows.Forms.Button()
+        Me.PnlBarra.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PnlBarra
+        '
+        Me.PnlBarra.BackColor = System.Drawing.Color.PeachPuff
+        Me.PnlBarra.Controls.Add(Me.LblBienvenida)
+        Me.PnlBarra.Controls.Add(Me.BtnMin)
+        Me.PnlBarra.Controls.Add(Me.BtnCerrar)
+        Me.PnlBarra.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PnlBarra.Location = New System.Drawing.Point(0, 0)
+        Me.PnlBarra.Name = "PnlBarra"
+        Me.PnlBarra.Size = New System.Drawing.Size(575, 40)
+        Me.PnlBarra.TabIndex = 9
+        '
+        'LblBienvenida
+        '
+        Me.LblBienvenida.AutoSize = True
+        Me.LblBienvenida.Font = New System.Drawing.Font("Montserrat", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblBienvenida.ForeColor = System.Drawing.Color.Black
+        Me.LblBienvenida.Location = New System.Drawing.Point(12, 5)
+        Me.LblBienvenida.Name = "LblBienvenida"
+        Me.LblBienvenida.Size = New System.Drawing.Size(363, 26)
+        Me.LblBienvenida.TabIndex = 11
+        Me.LblBienvenida.Text = "Biblioteca Municipal ""Maria E. Walsh"""
+        '
+        'BtnMin
+        '
+        Me.BtnMin.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnMin.FlatAppearance.BorderSize = 0
+        Me.BtnMin.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray
+        Me.BtnMin.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(238, Byte), Integer), CType(CType(205, Byte), Integer))
+        Me.BtnMin.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnMin.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnMin.ForeColor = System.Drawing.Color.LightGray
+        Me.BtnMin.Image = Global.Biblioteca.My.Resources.Resources.Icono_Minimizar_negro
+        Me.BtnMin.Location = New System.Drawing.Point(495, 0)
+        Me.BtnMin.Name = "BtnMin"
+        Me.BtnMin.Size = New System.Drawing.Size(40, 40)
+        Me.BtnMin.TabIndex = 6
+        Me.BtnMin.UseVisualStyleBackColor = True
+        '
+        'BtnCerrar
+        '
+        Me.BtnCerrar.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnCerrar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(85, Byte), Integer), CType(CType(159, Byte), Integer), CType(CType(127, Byte), Integer))
+        Me.BtnCerrar.FlatAppearance.BorderSize = 0
+        Me.BtnCerrar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray
+        Me.BtnCerrar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red
+        Me.BtnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnCerrar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnCerrar.ForeColor = System.Drawing.Color.LightGray
+        Me.BtnCerrar.Image = Global.Biblioteca.My.Resources.Resources.Icono_cerrar_FN_negro
+        Me.BtnCerrar.Location = New System.Drawing.Point(535, 0)
+        Me.BtnCerrar.Name = "BtnCerrar"
+        Me.BtnCerrar.Size = New System.Drawing.Size(40, 40)
+        Me.BtnCerrar.TabIndex = 7
+        Me.BtnCerrar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(20, Byte), Integer), CType(CType(57, Byte), Integer), CType(CType(80, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(575, 349)
+        Me.Controls.Add(Me.PnlBarra)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.PnlBarra.ResumeLayout(False)
+        Me.PnlBarra.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents PnlBarra As System.Windows.Forms.Panel
+    Friend WithEvents LblBienvenida As System.Windows.Forms.Label
+    Friend WithEvents BtnMin As System.Windows.Forms.Button
+    Friend WithEvents BtnCerrar As System.Windows.Forms.Button
+
+End Class
